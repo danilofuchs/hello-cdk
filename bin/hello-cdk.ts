fcb4425baa2b77b010d9aaec4e50dc08d8c10239
Danilo Fuchs <danilo_fuchs@hotmail.com>
2019-07-13 13:19:48 -0300

    Configure lambdas

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
-import cdk = require('@aws-cdk/core');
-import { HelloCdkStack } from '../lib/hello-cdk-stack';
+import cdk = require("@aws-cdk/core");
+import { HelloCdkStack } from "../lib/hello-cdk-stack";
 
 const app = new cdk.App();
-new HelloCdkStack(app, 'HelloCdkStack');
+new HelloCdkStack(app, "HelloCdkStack", {
+  env: {
+    region: "us-east-1",
+    account: "286144860510"
+  }
+});
--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,18 +1,29 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
-import cdk = require('@aws-cdk/core');
+import sqs = require("@aws-cdk/aws-sqs");
+import cdk = require("@aws-cdk/core");
+import lambda = require("@aws-cdk/aws-lambda");
+import { SqsEventSource } from "@aws-cdk/aws-lambda-event-sources";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'HelloCdkQueue', {
+    const queue = new sqs.Queue(this, "HelloCdkQueue", {
       visibilityTimeout: cdk.Duration.seconds(300)
     });
 
-    const topic = new sns.Topic(this, 'HelloCdkTopic');
+    const logQueue = new lambda.Function(this, "LogQueue", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.asset("./log"),
+      handler: "index.handler"
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const logQueuePy = new lambda.Function(this, "LogQueuePy", {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      code: lambda.Code.asset("./log"),
+      handler: "log.handler"
+    });
+
+    logQueue.addEventSource(new SqsEventSource(queue));
+    logQueuePy.addEventSource(new SqsEventSource(queue));
   }
 }
